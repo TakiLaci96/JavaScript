206650bc5a090eb1a6d5097120fffea6d3567c1f
TakiLaci96 <62886103+TakiLaci96@users.noreply.github.com>
2023-09-10 22:21:21 +0200

    Add files via upload

---
+++ feladat3.js
@@ -0,0 +1,12 @@
+var lista = [0,7,16,30,41];
+
+
+for (let i = lista.length - 1; i >= 0; i--) {
+    console.log(lista[i]);
+}
+
+var fordítottLista = lista.reverse();
+
+for (let i = 0; i < fordítottLista.length; i++) {
+    console.log(fordítottLista[i]);
+}
